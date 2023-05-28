15e4ef5e0a3f2a6dbdb5c4c835970358d7a4b5af
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-05-28 20:35:19 +0300

    add html parts

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 //Embedded JavaScript templates ejs
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
