1e781fda783df95b1722373de1b33edcb71bc352
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-05-28 17:44:37 +0300

    third part - ejs

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const express = require("express");
 
 const app = express();
+//Embedded JavaScript templates ejs
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index");
 });
 
+//making a maine page not from views
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
 app.get("/about", (req, res) => {
-  res.send("About");
+  res.render("about");
+});
+
+//making about page
+// app.get("/about", (req, res) => {
+//   res.send("About");
+// });
+
+app.get("/user/:username", (req, res) => {
+  let data = {
+    username: req.params.username,
+    hobbies: ["football", "scate", "bascketball"],
+  };
+  res.render("user", data);
 });
 
 //watchind dinamick name of url
-app.get("/user/:username/:id", (req, res) => {
-  res.send(`User ID: ${req.params.id}. Username: ${req.params.username}`);
-});
+// app.get("/user/:username/:id", (req, res) => {
+//   res.send(`User ID: ${req.params.id}. Username: ${req.params.username}`);
+// });
 
 const PORT = 3000;
 app.listen(PORT, () => {
